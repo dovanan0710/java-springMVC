4c96e460c083037f62190bf7a638e4934a27ad99
Do Van An <an07102003@gmail.com>
2024-06-11 16:16:56 +0700

    HelloController

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -3,9 +3,9 @@ package vn.hoidanit.laptopshop;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@RestController // beans
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/") // rảng buộc kết nối
     public String index() {
         return "Hello World from Spring Boot DO VAN AN!";
     }
