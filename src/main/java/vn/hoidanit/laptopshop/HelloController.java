b07fe1e12c1a134c84a27e15dfc28aeac8edfb81
Do Van An <an07102003@gmail.com>
2024-07-23 15:48:02 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/") // rảng buộc kết nối
     public String index() {
-        return "Hello World from Spring Boot DO VAN AN!";
+        return "Hello World from Spring Boot";
+    }
+
+    @GetMapping("/user") // rảng buộc kết nối
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin") // rảng buộc kết nối
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
