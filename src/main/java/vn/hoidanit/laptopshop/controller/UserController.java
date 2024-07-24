5ca5ff9529d66701c01616e94ed9a13d357dfed1
Do Van An <an07102003@gmail.com>
2024-07-24 11:48:04 +0700

    update 24/7

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,12 +1,12 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller // Annotation
@@ -19,24 +19,23 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());// them thuoc tinh key va gia tri
+        model.addAttribute("hoidanit", "From java hehe");
         return "hello";// tu cau hinh duoi jsp file hello
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
+    }
+
 }
-
-// @RestController // Annotation
-// // // funtion ứng với từng view
-// public class UserController {
-// // code theo DI
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping(" ")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
