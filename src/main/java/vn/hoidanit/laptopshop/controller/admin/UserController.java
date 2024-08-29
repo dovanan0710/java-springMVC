cf3b2455b1e08000b844e2de7e50ddc6909d4219
Do Van An <an07102003@gmail.com>
2024-08-29 07:43:57 +0700

    init

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -1,52 +1,47 @@
-package vn.hoidanit.laptopshop.controller;
+package vn.hoidanit.laptopshop.controller.admin;
 
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.*;
 import vn.hoidanit.laptopshop.domain.User;
-import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller // Annotation
-// funtion ứng với từng view mo hinh mvc
+@Controller
 public class UserController {
+
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(
+            UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        List<User> arrUser = this.userService.getAllUsersByEmail("anh07102003@gmail.com");
-        System.out.println(arrUser);
+        List<User> arrUsers = this.userService.getAllUsersByEmail("1@gmail.com");
+        System.out.println(arrUsers);
 
-        model.addAttribute("newUser", "test");// them thuoc tinh key va gia tri
-        model.addAttribute("hoidanit", "From java hehe");
-        return "hello";// tu cau hinh duoi jsp file hello
+        model.addAttribute("eric", "test");
+        model.addAttribute("hoidanit", "from controller with model");
+        return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         List<User> users = this.userService.getAllUsers();
         model.addAttribute("users1", users);
-        return "admin/user/table-user";
+        return "admin/user/show";
     }
 
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        this.userService.handleSaveUser(hoidanit);
-        return "redirect:/admin/user";
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "admin/user/detail";
     }
 
     @RequestMapping("/admin/user/create") // GET
@@ -55,15 +50,13 @@ public class UserController {
         return "admin/user/create";
     }
 
-    @RequestMapping("/admin/user/{id}")
-    public String getUserDetailPage(Model model, @PathVariable long id) {
-        User user = this.userService.getUserById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("id", id);
-        return "admin/user/show";
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        this.userService.handleSaveUser(hoidanit);
+        return "redirect:/admin/user";
     }
 
-    @RequestMapping("/admin/user/update/{id}")
+    @RequestMapping("/admin/user/update/{id}") // GET
     public String getUpdateUserPage(Model model, @PathVariable long id) {
         User currentUser = this.userService.getUserById(id);
         model.addAttribute("newUser", currentUser);
@@ -78,6 +71,7 @@ public class UserController {
             currentUser.setFullName(hoidanit.getFullName());
             currentUser.setPhone(hoidanit.getPhone());
 
+            // bug here
             this.userService.handleSaveUser(currentUser);
         }
         return "redirect:/admin/user";
@@ -86,7 +80,7 @@ public class UserController {
     @GetMapping("/admin/user/delete/{id}")
     public String getDeleteUserPage(Model model, @PathVariable long id) {
         model.addAttribute("id", id);
-        // User user=new User();
+        // User user = new User();
         // user.setId(id);
         model.addAttribute("newUser", new User());
         return "admin/user/delete";
@@ -97,5 +91,4 @@ public class UserController {
         this.userService.deleteAUser(eric.getId());
         return "redirect:/admin/user";
     }
-
 }
