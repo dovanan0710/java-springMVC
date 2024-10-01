10a4364166b0fd15b1e5871a8eb3cc5a7d4bf3ba
Do Van An <an07102003@gmail.com>
2024-10-01 10:03:31 +0700

    init

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import vn.hoidanit.laptopshop.domain.User;
 
 @Controller
-public class HomePageController {
-    @GetMapping("/")
-    public String getHomPage() {
-        return "client/HomePage/show";
+public class ItemController {
+
+    @GetMapping("/product/{id}")
+    public String getProductPage(Model model, @PathVariable long id) {
+        return "client/product/detail";
     }
+
 }
